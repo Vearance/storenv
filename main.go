12d07a642cd70d9db45b0b4c75d30d9819ef8fee
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-02-10 11:00:39 +0700

    http

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
+import "net/http"
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>Hello, World!<h1>")
+	}) 
+
+	http.ListenAndServe(":80", nil)
 }
